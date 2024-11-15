ae11afd4864cc22f66905313c13ac96f3368c12f
CarlYarwood <cdy8858@truman.edu>
2024-11-15 15:41:27 -0600

    updated repl to use parser as it is the current latest stage in the pipeline

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
     "fmt"
     "io"
     "monkey/lexer"
-    "monkey/token"
+    "monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -22,10 +22,25 @@ func Start(in io.Reader, out io.Writer) {
 
         line := scanner.Text()
         l := lexer.New(line)
+        p := parser.New(l)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-            fmt.Fprintf(out, "%v\n", tok)
+        program := p.ParseProgram()
+
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
         }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
+    }
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, "oops! we ran into an error here!\n")
+    io.WriteString(out, " parser errrs: \n")
+    for _, msg := range errors {
+        io.WriteString(out, "\t" + msg + "\n")
     }
 }
 
